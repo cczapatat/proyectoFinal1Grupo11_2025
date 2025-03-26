86d03735f85eb36a0ddffc08e641e9a087a05ddb
jctorresm1 <jc.torresm1@uniandes.edu.co>
2025-03-26 00:56:19 -0500

    revert

--- user_session_manager/dtos/user_session_dto.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass
-class UserSessionDTO:
-    email: str = field(default=None)
-    password: str = field(default=None)
-    type: str = field(default=None)
--- user_session_manager/models/data_classes.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class LoginIn:
-    id: int
-    id_usuario: int = 0
-    type: str = 'SELLER'
