7a72aeaf66aef496606a97e70cc80532d663798b
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-03-29 11:49:01 -0500

    [HU-09] change DB pass user

--- client/config/db.py
+++ client/config/db.py
@@ -24,7 +24,7 @@ def create_db(app: Flask):
 def get_uri_db() -> str:
     host = os.getenv('DB_HOST', default="127.0.0.1")
     port = os.getenv('DB_PORT', default="5432")
-    user = os.getenv('DB_USER', default="postgres")
+    user = os.getenv('DB_USER', default="user_final")
     password = os.getenv('DB_PASSWORD', default="pass_final")
     db_name = os.getenv('DB_NAME', default="project_final")
     db_type = os.getenv('DB_TYPE', default="postgresql")
