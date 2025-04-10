08ef1047b4e2b86749167a8dfee7d1f1cabd7590
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-04-09 23:43:28 -0500

    [HU-16] Add test bed

--- web-smartstock/src/app/services/client.service.spec.ts
+++ web-smartstock/src/app/services/client.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ClientService } from './client.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ClientService', () => {
   let service: ClientService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ClientService);
   });
 
