8eeba9e595e2be6581bcb261c2350d18f30d15f1
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-04-13 08:50:16 -0500

    [HU-16] Add cypress test

---
+++ web-smartstock/cypress/e2e/salespeople/list-clients/seller-list-clients.cy.ts
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker';
+
+describe('List Seller´s clients Test', () => {
+  it('Create New Seller', () => {
+    cy.visit('/user-sessions/login');
+    cy.get('#email').type('admin.admin@sta.com');
+    cy.get('#password').type('123456');
+    cy.get('#doLogin').click();
+    cy.url().should('include', '/home')
+
+    cy.get('.menu-toggle').click();
+    cy.get('#nav_languages').click();
+    cy.get('#nav_es-CO').click();
+    cy.get('#nav_customers').click();
+    cy.get('#nav_customer_list').click();
+  
+    cy.contains('acypress2@sta.com');
+    cy.contains('td', 'acypress2@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+    cy.contains('aclientcypress@sta.com');
+
+    
+    cy.contains('acypress3@sta.com');
+    cy.contains('td', 'acypress3@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+
+    cy.get('#empty-customer').should('be.visible');
+
+    cy.get('.menu-toggle').click()
+    cy.get('.btn-logout').click()
+    cy.url().should('include', '/user-sessions/login')
+    
+  });
+});
