f7f2954559dded1656a9704903dfa2e2be2e4025
jctorresm1 <jc.torresm1@uniandes.edu.co>
2025-04-23 21:15:22 -0500

    [HU-036] Video API & Video Recomendation Worker

--- video_recommendations_worker/app/core/db.py
+++ video_recommendations_worker/app/core/db.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Database connection settings
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_USER = os.environ.get('DB_USER', 'user_final"')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'pass_final')
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "project_final")
