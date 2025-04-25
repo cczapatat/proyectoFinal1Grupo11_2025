ef3c1bac5b76ef5a6af73455dc930c2e5d28bb2d
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-04-25 18:49:14 -0500

    [HU-16] Adiciona test cypress para asignarClienteVendedor

---
+++ web-smartstock/cypress/e2e/salespeople/assing-customer/assign-customer.cy.ts
@@ -0,0 +1,91 @@
+import { faker } from '@faker-js/faker';
+
+describe('Associale Seller to clients Test', () => {
+  it('Associate Sellers', () => {
+    cy.visit('/user-sessions/login');
+    cy.get('#email').type('admin.admin@sta.com');
+    cy.get('#password').type('123456');
+    cy.get('#doLogin').click();
+    cy.url().should('include', '/home')
+
+    cy.get('.menu-toggle').click();
+    cy.get('#nav_languages').click();
+    cy.get('#nav_es-CO').click();
+    cy.get('#nav_assign_customers').click();
+
+  
+    cy.contains('jhon1@sta.com');
+    cy.contains('td', 'jhon1@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+
+    cy.contains('aclientcypress@sta.com');
+    cy.contains('td', 'aclientcypress@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+    cy.contains('aclientcypress1@sta.com');
+    cy.contains('td', 'aclientcypress1@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+    cy.contains('aclientcypress2@sta.com');
+    cy.contains('td', 'aclientcypress2@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+    
+    cy.contains('aclientcypress3@sta.com');
+    cy.contains('td', 'aclientcypress3@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+
+    cy.get('#associate-save').click();  //save
+
+
+    cy.contains('seller1@sta.com');
+    cy.contains('td', 'seller1@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+    cy.contains('jhon1@sta.com');
+    cy.contains('td', 'jhon1@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .check(); 
+
+
+    cy.contains('td', 'aclientcypress@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .should('be.checked'); 
+
+    cy.contains('td', 'aclientcypress1@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .should('be.checked'); 
+
+    cy.contains('td', 'aclientcypress2@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .should('be.checked'); 
+
+    cy.contains('td', 'aclientcypress3@sta.com') // find the cell with the target email
+    .parent('tr')                         // go to the parent row
+    .find('input[type="checkbox"]')      // find the checkbox in that row
+    .should('be.checked'); 
+
+
+    cy.get('.menu-toggle').click()
+    cy.get('.btn-logout').click()
+    cy.url().should('include', '/user-sessions/login')
+    
+  });
+});
