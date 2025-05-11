fa2eae286a6db8f277fc72644eb8591d1a9c23b6
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-05-10 22:40:54 -0500

    [HU-62] fix credentials 1

--- stocks_api/config/cache.py
+++ stocks_api/config/cache.py
@@ -2,7 +2,7 @@ import os
 
 import redis
 
-REDIS_HOST = os.environ.get("REDIS_HOST", "35.222.147.187")
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 REDIS_DB = int(os.environ.get("REDIS_DB", 0))
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
